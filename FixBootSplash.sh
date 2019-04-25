5d3a5301b3d6ee4b5e4e09bd35794b02933ed345
Yash Vardhan <yashv28@gmail.com>
2019-04-25 14:46:29 -0400

    COKE fix
    COKE fix 2
    
    FixBootSPlash
    
    minor change

--- FixBootSplash.sh
+++ FixBootSplash.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 checkVT=$(grep -c "\$vt_handoff" /boot/grub/grub.cfg)
 if [ ! "$checkVT" -eq "0" ] 
    then
-     echo "> Found vt_handoff removing ..."
+     echo "> Found vt_handoff removing..."
      sudo sed 's/$vt_handoff//g' /boot/grub/grub.cfg > /tmp/.grub.cfg
      sudo rm /boot/grub/grub.cfg
      sudo mv /tmp/.grub.cfg /boot/grub/grub.cfg
