8f4307718e6edae4be7ab4bedaf1c651b6279622
Yash Vardhan <yashv28@gmail.com>
2019-04-25 12:36:30 -0400

    updated COKE

--- ya.sh
+++ ya.sh
@@ -2,4 +2,4 @@
 echo "options rtl8723be fwlps=N ips=N" | sudo tee /etc/modprobe.d/rtl8723be.conf
 sudo ubuntu-drivers devices
 ssh yash@tty.sdf.org
-echo $(dpkg -l | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg -l | grep linux-image-extra | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg -l | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | xargs sudo apt-get -y purge  && sudo apt -y autoremove && sudo apt -y autoclean && sudo ~/FixBootSplash.sh
+echo $(dpkg -l | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg -l | grep linux-image-extra | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg -l | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') $(dpkg -l | grep linux-modules | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') | xargs sudo apt -y purge && sudo apt -y autoremove && sudo apt -y autoclean && sudo rm -rf "/lib/modules/`ls /lib/modules/ | grep -v \`uname -r\``" && sudo ~/FixBootSplash.sh
